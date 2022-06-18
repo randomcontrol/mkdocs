59248cc34e91d140e7f9600c34a6f694dbca18c8
chemaguerra <chema@maverickrender.com>
2022-06-18 22:32:09 +0200

    initial

---
+++ win_activate.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if "%1" == "clean" rmdir /s venv
+
+if not exist "venv" (
+  mkdir venv
+  python -m venv venv
+     venv\Scripts\activate
+    "venv\Scripts\python" -m pip install --upgrade pip
+     pip install mkdocs
+) else (
+  venv\Scripts\activate
+)
---
+++ win_compress.bat
@@ -0,0 +1,24 @@
+@echo off
+@setlocal EnableDelayedExpansion
+
+rem #################################
+rem ## compress.bat | Chema Guerra ##
+rem #################################
+
+set __RAR=Z:\RandomControl - toolchain\winrar\winrar.exe
+
+rem #####################
+rem ## Pick the files. ##
+rem #####################
+
+echo **NOT** deleting temporary files...
+
+echo Compressing mk-docs files...
+
+"%__RAR%" a -r -afzip -m5                                    ^
+                                                             ^
+    -ag" - yyyy.mm.dd - hh.mm" "RandomControl - mk-docs.zip" ^
+                                                             ^
+    ".\01. lib\*"                                            ^
+    ".\02. api\*"                                            ^
+    ".\03. bat\*" win_activate.bat win_deactivate.bat >NUL
